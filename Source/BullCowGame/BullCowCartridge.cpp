2523493310e6f73eeba355ec1312e852976236e9
Justin Knife <jkni3047@uni.sydney.edu.au>
2021-03-31 07:04:42 +1100

    na

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,20 +4,59 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-    PrintLine(TEXT("Welcome to the Bull Cow Game!"));
-    PrintLine(TEXT("Please guess the 5 letter word:")); // Make number programmatic
+
+    SetupGame();
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    FString HiddenWord = TEXT("shore"); // Move outside of this function
-    if (Input == HiddenWord)
+
+    if (bGameOver)
     {
-        PrintLine(TEXT("Congratulations!"));
+        ClearScreen();
+        SetupGame()
     }
-    else
-    {
-        PrintLine(TEXT("Try again."));
+    else {
+        if (Input == HiddenWord)
+        {
+            PrintLine(TEXT("Congratulations!"));
+            PrintLine(TEXT("The hidden word was '%s'."), *HiddenWord);
+            EndGame();
+        }
+        else
+        {
+            if  (HiddenWord.Len() != Input.Len())
+            {
+                PrintLine(TEXT("Incorrect word length."));
+                EndGame();
+            }
+        }
     }
+
+    // Check isogram
+    // Check number of characters
+
+    // Remove life
+    // Check lives remaining
+
+
+
+}
+
+void UBullCowCartridge::SetupGame()
+{
+    HiddenWord = TEXT("shore");
+    GameLives = 5;
+    bGameOver = false;
+
+    PrintLine(TEXT("Welcome to the Bull Cow Game!"));
+    PrintLine(TEXT("The hidden word is %i characters long."), HiddenWord.Len());
+    PrintLine(TEXT("Please enter your guess..."));
+}
+
+void UBullCowCartridge::EndGame()
+{
+    bGameOver = true;
+    PrintLine(TEXT("Press enter to play again..."));
 }
